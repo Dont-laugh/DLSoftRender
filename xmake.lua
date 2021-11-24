14e25c8517225d0a66de964a9970e91a0ce6413a
莫笑 <2387471378@qq.com>
2021-11-24 17:49:14 +0800

    Complete Mac OS Enviornment

--- xmake.lua
+++ xmake.lua
@@ -1,19 +1,20 @@
 add_rules("mode.debug", "mode.release")
 add_cxxflags("-std=c++14")
 
+-- SDL2
+if is_os("windows") then
+	add_ldflags("/SUBSYSTEM:CONSOLE")
+	add_links("Shell32")
+	add_includedirs("third_party/SDL2-2.0.16/include")
+	add_linkdirs("third_party/SDL2-2.0.16/lib")
+	add_links("SDL2main", "SDL2")
+elseif is_os("macosx") then
+	add_requires("brew::sdl2 2.0.16", { alias = "sdl2" })
+	add_packages("sdl2")
+end
+
+-- Main Renderer
 target("DLSoftRenderer")
 	set_kind("binary")
 	add_includedirs("src/include")
 	add_files("src/*.cpp")
-
-	add_includedirs("third_party/SDL2-2.0.16/include")
-
-	if is_os("windows") then
-		add_ldflags("/SUBSYSTEM:CONSOLE")
-		add_links("Shell32")
-		add_linkdirs("third_party/SDL2-2.0.16/lib/win")
-	elseif is_os("macosx") then
-		add_linkdirs("third_party/SDL2-2.0.16/lib/mac")
-	end
-
-	add_links("SDL2main", "SDL2")
